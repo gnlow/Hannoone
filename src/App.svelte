a11a67c8c000e9426c06adaf11698937d7e88359
Gnlow <entrydark@gmail.com>
2020-08-20 23:02:32 +0900

    change

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,26 @@
+<style>
+    main {
+        font-family: sans-serif;
+        margin: 0 auto;
+        max-width: 1400px;
+        min-height: 100vh;
+        padding: 10px;
+        color: white;
+    }
+    header {
+        font-size: 1em;
+        line-height: 1em;
+    }
+    section {
+        padding: 50px;
+        background-color: white;
+        color: black;
+        border-radius: 30px;
+        box-shadow: 0 5px 20px rgba(0, 0, 0, 16%);
+        margin: 50px;
+    }
+</style>
+
 <script>
     import { onMount } from "svelte";
     export let date;
