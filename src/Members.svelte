72d6bed0f1bb6ac7aa3538a7c0e8f01cba40ad5d
Gnlow <entrydark@gmail.com>
2020-08-31 23:36:40 +0900

    members sort

--- src/Members.svelte
+++ src/Members.svelte
@@ -15,9 +15,23 @@
 <script>
     import parties from "./parties.js"
     export let localInfo
+
+    const order = members => {
+        let partyMap = {}
+        Object.keys(parties).forEach((party, i) => partyMap[party] = i)
+
+        let result = Array(Object.keys(parties).length).fill().map(x => [])
+        console.log(result, partyMap)
+        members.forEach(member => {
+            result[partyMap[member.party]].push(member)
+        })
+        return result.flat()
+    }
+
+    const members = order(localInfo.members)
 </script>
 <ul>
-    {#each localInfo.members as { name, party }, i}
+    {#each members as { name, party }, i}
         <li style="grid-column: {i % 10 + 1};">
             <div style="
                 background: {parties[party].color.primary};
